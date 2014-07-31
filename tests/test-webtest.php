bf9b54461f39fc600fdd88ffe5751dd0449d1979
pieroit <savastanos@libero.it>
2014-07-31 13:49:21 +0200

    sauce-travis integration

--- tests/test-webtest.php
+++ tests/test-webtest.php
@@ -3,7 +3,7 @@ class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
     protected function setUp()
     {
-        $this->setBrowser('chrome');
+        $this->setBrowser('firefox');
         $this->setBrowserUrl('http://www.example.com/');
     }
 
